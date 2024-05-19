5fbffebecccc77715304a89d93eece7c3f4f30d9
NRG-Drink <beatdz93@hotmail.com>
2024-05-19 20:45:20 +0200

    chore: write logic to find and print out user and calendar data

---
+++ NRG.CalendarFinder/NRG.CalendarFinder/CalendarFinderService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using NRG.CalendarFinder.MsGraphFactories;
+
+namespace NRG.CalendarFinder;
+
+public class CalendarFinderService(IMsGraphClientFactory graphFactory)
+{
+    private readonly GraphServiceClient _graph = graphFactory.GetClient("GraphClient");
+
+    public async Task<User> FindUserAsyncOrThrow(string userIdentifier)
+    {
+        if (string.IsNullOrWhiteSpace(userIdentifier))
+        {
+            throw new ArgumentException(userIdentifier, nameof(userIdentifier));
+        }
+
+        var user = await _graph.Users[userIdentifier].GetAsync();
+
+        if (user is null || user.Id is null)
+        {
+            throw new ArgumentException($"Found user is not valid for identifier: {userIdentifier}");
+        }
+
+        return user;
+    }
+
+    public async Task<List<Calendar>> FindCalendarsAsyncOrThrow(User user)
+    {
+        var calendarResponse = await _graph.Users[user.Id ?? user.Mail].Calendars.GetAsync();
+
+        if (calendarResponse?.Value is null)
+        {
+            throw new ArgumentException("Found calendars ar not valid.");
+        }
+
+        return calendarResponse.Value;
+    }
+}
---
+++ NRG.CalendarFinder/NRG.CalendarFinder/CalendarFinderWorker.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Graph.Models;
+using Microsoft.Graph.Models.ODataErrors;
+using NRG.CalendarFinder.Models;
+using System.Text.Json;
+
+namespace NRG.CalendarFinder;
+
+public class CalendarFinderWorker(
+    IHost host,
+    CalendarFinderService finder,
+    Options options
+    )
+    : BackgroundService
+{
+
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            await WriteObjectAsync("Query", options);
+
+            var user = await finder.FindUserAsyncOrThrow(options.UserIdentifier);
+            var calendars = await finder.FindCalendarsAsyncOrThrow(user);
+
+            await WriteObjectAsync("Result", GetPrintObject(user, calendars));
+        }
+        catch (ODataError ex)
+        {
+            await Console.Out.WriteLineAsync($"Failed with {nameof(ODataError)}: {ex.Error?.Message}");
+        }
+        catch (Exception ex)
+        {
+            await Console.Out.WriteLineAsync($"Failed with error: {ex.Message}");
+        }
+        finally
+        {
+            await host.StopAsync(stoppingToken);
+        }
+    }
+
+    private static object GetPrintObject(User user, List<Calendar> calendars)
+        => new
+        {
+            User = new { user.Id, user.Mail },
+            Calendars = calendars.Select(e => new { e.Id, e.Name, Owner = e.Owner?.Address })
+        };
+
+    private static Task WriteObjectAsync(string text, object? obj)
+    {
+        var json = JsonSerializer.Serialize(obj, options: new() { WriteIndented = true });
+        return Console.Out.WriteLineAsync($"{text}:\n{json}");
+    }
+}
